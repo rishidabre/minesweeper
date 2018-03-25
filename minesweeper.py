4b113f4a0d839b7603e3d966bd38684302a562c2
rishi <rishidabre@gmail.com>
2018-03-25 18:01:33 -0400

    First complete commit of Minesweeper for terminal.

---
+++ minesweeper.py
@@ -0,0 +1,243 @@
+#!/usr/bin/python
+
+import random
+from os import system
+import signal
+
+# Main class for grid
+class GameBoard:
+
+	# matrix - Main grid, 2 dimensional matrix
+	# rows - Total rows
+	# columns - Total columns
+	# mines - Total mines
+	# mine_cells - List of (x, y) tuples where mines are placed
+	# left = Total safe cells left to mark (rows * columns - mines)
+
+	# Each cell in grid
+	class Cell:
+
+		# value - Total number of adjacent cells with mines
+		# state - Marked (1) or not (0)
+		# mine - Has mine (1) or not (0)
+
+		# Cell constructor
+		def __init__(self, value, state):
+			self.value = value
+			self.state = state
+			self.mine = False
+
+	# GameBoard constructor
+	def __init__(self, rows, columns, mines):
+		self.rows = rows
+		self.columns = columns
+		self.mines = mines
+		self.left = (rows * columns - mines)
+		self.__initialize()
+
+	# Initializes the grid by placing mines randomly and updating values of adjacent cells
+	def __initialize(self):
+		# Initialize all 'columns' number of cells for each row from 'rows'
+		self.matrix = [[self.Cell(0, 0) for _ in xrange(self.columns)] for _ in xrange(self.rows)]
+		self.mine_cells = []
+		for _ in xrange(self.mines):
+			x = random.randint(0, self.columns - 1)
+			y = random.randint(0, self.rows - 1)
+			# Repeat until new cell (where mine isn't already placed) is found
+			while self.matrix[y][x].mine:
+				x = random.randint(0, self.columns - 1)
+				y = random.randint(0, self.rows - 1)
+			# Add to mine_cells list and update in matrix
+			self.mine_cells.append((x, y))
+			self.matrix[y][x].mine = True
+		# Update adjacent cells of mine cells
+		self.__update_mine_territory()
+
+	# Updates the adjacent cells of mine cells with appropriate values
+	def __update_mine_territory(self):
+		# For each mine cell
+		for m in self.mine_cells:
+			v = self.matrix[m[1]][m[0]]
+			# Update all adjacent (x - 1, y - 1) through (x + 1, y + 1) cells
+			for x in xrange(m[0] - 1, m[0] + 2):
+				for y in xrange(m[1] - 1, m[1] + 2):
+					if self.check_limits(x, y):
+						continue
+					c = self.matrix[y][x]
+					# Only update the value if it's not a mine cell and is not marked
+					if not self.matrix[y][x].mine and not c.state:
+						self.matrix[y][x].value += 1
+			self.matrix[m[1]][m[0]] = v
+
+	# Marks the neighbors of given cell recursively as applicable
+	def __mark_neighbors(self, col, row):
+		if self.check_limits(col, row):
+			return
+		# Skip if contains mine
+		if self.matrix[row][col].mine:
+			return
+		# Skip if already marked
+		if self.matrix[row][col].state:
+			return
+		# Mark this cell and dDecrement count of unmarked cells
+		self.matrix[row][col].state = 1
+		self.left -= 1
+		# If this cell surrounds no cells with mines, continue to mark neighbors
+		if self.matrix[row][col].value:
+			return
+		# Maintain list of safe neighbors, these will be marked recursively
+		safe_neighbors = []
+		# For all adjacent cells (x - 1, y - 1) through (x + 1, y + 1)
+		for x in xrange(col - 1, col + 2):
+			for y in xrange(row - 1, row + 2):
+				if self.check_limits(x, y):
+					continue
+				# Add neighbor if it has no mine
+				if not self.matrix[y][x].mine:
+					safe_neighbors.append((x, y))
+					continue
+				# Break if it has
+				break
+		# Remove the current cell from safe_neighbors list
+		p = (col, row)
+		if p in safe_neighbors:
+			safe_neighbors.remove(p)
+		# Mark all the safe neighbors recursively
+		for n in safe_neighbors:
+			self.__mark_neighbors(n[0], n[1])
+		return
+
+	# Marks given cell and any neighbors (recursively) if it's value is 0
+	def mark_cell(self, col, row):
+		# Skip if already marked
+		if self.matrix[row][col].state:
+			return
+		# Check if it has mine
+		if self.matrix[row][col].mine:
+			# Mine hit, reveal all mines and return True
+			for x, y in self.mine_cells:
+				self.matrix[y][x].state = 1
+			return True
+		# If cell value if non-zero, just mark it and return
+		if self.matrix[row][col].value:
+			self.left -= 1
+			self.matrix[row][col].state = 1
+			return False
+		# Cell value is 0, recursively mark neighbors
+		self.__mark_neighbors(col, row)
+		return False
+
+	# Prints the entire board on console (real = True if mines are to be revealed)
+	def print_board(self, real):
+		if real:
+			print map(lambda (x, y): (x + 1, y + 1), self.mine_cells)
+		print 
+		# Print row and column indices
+		print "%-2s"%0,
+		for i in xrange(self.columns):
+			print "%2s"%(i + 1),
+		print
+		i = 1
+		for x in xrange(self.rows):
+			print "%-2s"%i,
+			for y in xrange(self.columns):
+				if real or self.matrix[x][y].state:
+					if self.matrix[x][y].mine:
+						print " x",
+					else:
+						print "%2s"%self.matrix[x][y].value,
+				else:
+					print " -",
+			print
+			i += 1
+		print
+
+	# Checks whether given coordinates obey the limits
+	def check_limits(self, x, y):
+		if x < 0 or x >= self.columns or y < 0 or y >= self.rows:
+			return True
+		return False
+
+# Prompts user with given message and requests for a number repeatedly until it is valid
+def get_number(msg, default):
+	inv_msg = "Invalid number, try again"
+	while True:
+		try:
+			n = input(msg)
+			if not n:
+				print inv_msg
+				continue
+			n = int(n)
+			if n < 0:
+				print inv_msg
+				continue
+			return n
+		except SyntaxError:
+			return default
+		except StandardError:
+			print inv_msg
+
+# Invoked when interrupt is triggered and terminates game gracefully
+def signal_handler(signal, frame):
+	print "...game terminated."
+	quit()
+
+# Clears terminal and prints given message
+def clear_and_prompt(msg):
+	system('clear')
+	print msg
+
+# Validates given configuration
+def validate(rows, columns, mines):
+	return mines < (rows * columns)
+
+# Main function encapsulating game play
+def gameplay(board):
+	inv_indices_msg = "Invalid indices, try again"
+	clear_and_prompt("Let's begin...")
+	while True:
+		print "Safe cells left: ",board.left
+		board.print_board(False)
+		try:
+			i = input("Mark cell (col, row): ")
+			if not i or len(i) <> 2:
+				clear_and_prompt(inv_indices_msg)
+				continue
+			x = i[0]
+			y = i[1]
+			x = int(x)
+			y = int(y)
+			if(board.check_limits(x - 1, y - 1)):
+				clear_and_prompt(inv_indices_msg)
+				continue
+		except Exception:
+			clear_and_prompt(inv_indices_msg)
+			continue
+		if(board.mark_cell(x - 1, y - 1)):
+			clear_and_prompt("Stepped on a mine at [%d, %d]!"%(x, y))
+			board.print_board(False)
+			print "Game over!"
+			break
+		clear_and_prompt("Marked [%d, %d]... Safe!"%(x, y))
+		if not board.left:
+			board.print_board(False)
+			print "Congratulations, you win!"
+			break
+
+if __name__ == "__main__":
+	# Set handler for interrupt signal (^c)
+	signal.signal(signal.SIGINT, signal_handler)
+	print "Welcome to Minesweeper for terminal!"
+	print "Configure the mine grid to begin (skip for default)..."
+	# Get configuration
+	rows = get_number("Enter number of rows [16]: ", 16)
+	columns = get_number("Enter number of columns [16]: ", 16)
+	mines = get_number("Enter number of mines [40]: ", 40)
+	# Perform basic validation
+	if not validate(rows, columns, mines):
+		print "Invalid configuration"
+		quit()
+	# Initialize the board
+	board = GameBoard(rows, columns, mines)
+	# Start gameplay
+	gameplay(board)
